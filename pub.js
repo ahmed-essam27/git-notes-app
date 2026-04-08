2599da32760bb88c3cff42e587c56ec4e56f3294
ahmed-essam27 <ahmedmar3y7777@gmail.com>
2026-04-08 20:28:50 +0200

    feature: add delete button

--- pub.js
+++ pub.js
@@ -7,3 +7,15 @@ li.textContent = input.value;
 list.appendChild(li);
 input.value = "";
 }
+function addNote() {
+const input = document.getElementById("noteInput");
+const list = document.getElementById("list");
+const li = document.createElement("li");
+li.textContent = input.value;
+const btn = document.createElement("button");
+btn.textContent = "Delete";
+btn.onclick = () => li.remove();
+li.appendChild(btn);
+list.appendChild(li);
+input.value = "";
+}
